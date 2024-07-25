23d25f9c18d2ebbd9c44be52900e22abb66b0e72
ashwinumadi <ashwinumadi@users.noreply.github.com>
2024-07-25 23:57:33 +0000

    deploy: 2eec1f757948860c79e174ff2036b022330c5ac9

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Post number 2",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/bradley_lab/research_post/",
+        "url": "/Bradley-Lab/research_post/",
         "teaser": null
       },{
         "title": "Post number 1",
         "excerpt":"FIRST FIRST FIRST FIRSFTIDSITF ITRSIR FIRW GRI GI   Fill in the contents for the proposed post. Click here   ","categories": [],
         "tags": [],
-        "url": "/bradley_lab/research_post/",
+        "url": "/Bradley-Lab/research_post/",
         "teaser": null
       }]
